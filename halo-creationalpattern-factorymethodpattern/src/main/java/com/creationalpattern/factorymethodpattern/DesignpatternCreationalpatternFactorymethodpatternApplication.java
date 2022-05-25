4fbc66a55a069d80cb9720886a3d7963d2d4da24
yuwenbo <yuwenbo00@163.com>
2022-05-26 07:05:14 +0800

    [DESCRIPTION]: halo-creationalpattern-factorymethodpattern [MODULE]: ALL [BRANCH]: master

---
+++ halo-creationalpattern-factorymethodpattern/src/main/java/com/creationalpattern/factorymethodpattern/DesignpatternCreationalpatternFactorymethodpatternApplication.java
@@ -0,0 +1,13 @@
+package com.creationalpattern.factorymethodpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpatternCreationalpatternFactorymethodpatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignpatternCreationalpatternFactorymethodpatternApplication.class, args);
+    }
+
+}

b66b382ec3071d98a46c8d29162da6f7682c2393
yuwenbo <yuwenbo00@163.com>
2022-05-28 07:19:40 +0800

    [DESCRIPTION]: 工厂方法模式解耦代码-测试类验证。 [MODULE]: factory-method [BRANCH]: master

--- halo-creational-factorymethod/factorymethod-02/src/test/java/com/ilearning/design/test/ApiTest.java
+++ halo-creational-factorymethod/factorymethod-02/src/test/java/com/ilearning/design/test/ApiTest.java
@@ -7,36 +7,35 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.ilearning.design.enumtype.AwardTypeEnum.*;
+
 /**
- * 测试类
+ * Api测试类
  *
  * @author yuwenbo
  * @date 2022/5/27 23:33
  **/
 public class ApiTest {
+
     @Test
-    public void test_commodity() throws Exception {
+    public void test_award() {
         AwardFactory awardFactory = new AwardFactory();
 
         // 1. 优惠券
-        ICommodity commodityService_1 = awardFactory.getAwardService(1);
+        ICommodity commodityService_1 = awardFactory.getAwardService(COUPON.getEnvState());
         commodityService_1.sendAward("10001", "EGM1023938910232121323432", "791098764902132", null);
 
         // 2. 实物商品
-        ICommodity commodityService_2 = awardFactory.getAwardService(2);
+        ICommodity commodityService_2 = awardFactory.getAwardService(GOODS.getEnvState());
         Map<String, String> extMap = new HashMap<String, String>();
-        extMap.put("consigneeUserName", "飞机");
+        extMap.put("consigneeUserName", "实物商品");
         extMap.put("consigneeUserPhone", "15300002222");
         extMap.put("consigneeUserAddress", "陕西省.西安市.雁塔区.XX街道.小区.#18-2109");
 
-        commodityService_2.sendAward("10001", "9820198721311", "1023000020112221113", new HashMap<String, String>() {{
-            put("consigneeUserName", "飞机");
-            put("consigneeUserPhone", "15300003333");
-            put("consigneeUserAddress", "陕西省.西安市.雁塔区.XX街道.小区.#18-2109");
-        }});
+        commodityService_2.sendAward("10001", "9820198721311", "1023000020112221113", extMap);
 
         // 3. 第三方兑换卡(爱奇艺)
-        ICommodity commodityService_3 = awardFactory.getAwardService(3);
+        ICommodity commodityService_3 = awardFactory.getAwardService(CARD.getEnvState());
         commodityService_3.sendAward("10001", "AQY1xjkUodl8LO975GdfrYUio", null, null);
 
     }

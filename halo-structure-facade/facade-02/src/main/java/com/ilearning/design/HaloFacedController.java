f88fe9570e9a95fe64d587a80efa51770a6a9f9e
yuwenbo <yuwenbo00@163.com>
2022-07-09 22:38:27 +0800

    [DESCRIPTION]: [halo-structure-facade] add main progress [MODULE]: facade-02 [BRANCH]: master

---
+++ halo-structure-facade/facade-02/src/main/java/com/ilearning/design/HaloFacedController.java
@@ -0,0 +1,17 @@
+package com.ilearning.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主程序
+ *
+ * @author yuwenbo
+ * @date 2022/7/9 17:43
+ **/
+@SpringBootApplication
+public class HaloFacedController {
+    public static void main(String[] args) {
+        SpringApplication.run(HaloFacedController.class, args);
+    }
+}
